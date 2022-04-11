58b83fac077bc43942634ff59d03ed43dd300afe
François <fbouscaillou@myges.fr>
2022-04-12 01:56:50 +0200

    add texture

---
+++ Assets/Scripts/ElectricSource.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ElectricSource : ScriptableObject
+{
+    public string nameEnergy;
+    public float costKwh;
+    public float capacityKwh;
+    public float footPrint;
+}
