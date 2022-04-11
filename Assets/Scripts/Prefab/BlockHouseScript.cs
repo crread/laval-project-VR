c5e4ca2cb3eb01117530e84c5a8d5365517462c0
erwan-ash <60441163+erwan-ash@users.noreply.github.com>
2022-04-12 01:15:52 +0200

    save mickael works

---
+++ Assets/Scripts/Prefab/BlockHouseScript.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prefab
+{
+    public class BlockHouseScript : MonoBehaviour
+    {
+        public GameObject _door;
+        
+        public GameObject _MeshController;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("BlockHouse"))
+            {
+                _door.SetActive(false);
+                _MeshController.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Scene/SceneManager.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scene
+{
+    public class SceneManager : MonoBehaviour
+    {
+
+        [SerializeField] private List<GameObject> _boardToken;
+        
+        [SerializeField] public List<GameObject> _boardHouse;
+
+        [SerializeField] private GameObject _housePrefab;
+        
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+
+        private void BuildHouse()
+        {
+            
+        }
+    }
+}
