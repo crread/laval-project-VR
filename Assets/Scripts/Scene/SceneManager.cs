6438b24c086882afe91548689963458f314d52c5
unknown <mickaelberthelot@outlook.fr>
2022-04-12 09:59:51 +0200

    save

--- Assets/Scripts/Scene/SceneManager.cs
+++ Assets/Scripts/Scene/SceneManager.cs
@@ -8,13 +8,23 @@ namespace Scene
 
         [SerializeField] private List<GameObject> _boardToken;
         
-        [SerializeField] public List<GameObject> _boardHouse;
+        [SerializeField] private List<GameObject> _boardHouse;
 
         [SerializeField] private GameObject _housePrefab;
 
         void Update()
         {
         
+        }
+        
+        public int GetIndexBoardHousePosition(GameObject go)
+        {
+            return _boardHouse.IndexOf(go);
+        }
+        
+        public int GetIndexBoardTokenPosition(GameObject go)
+        {
+            return _boardToken.IndexOf(go);
         }
 
         private void BuildHouse(Vector3 position)
--- Assets/SnapCollision.cs
+++ Assets/SnapCollision.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Scene;
 using UnityEngine;
 using Valve.VR.InteractionSystem.Sample;
 
@@ -9,13 +10,20 @@ public class SnapCollision : MonoBehaviour
     public int id;
     public bool free;
 
+    public SceneManager sceneManager;
+        
     private void OnTriggerEnter(Collider other)
     {
         
         if (other.gameObject.CompareTag("Token"))
         {
+            var idx = sceneManager.GetIndexBoardTokenPosition(other.gameObject);
+            
+
             other.gameObject.GetComponent<LockToPoint>().snapTo = transform;
             Debug.Log("done");
+            
+            
         }
     }
 }
